064a20efb01c57793e99fa20abc9cecb8129c96f
Ashish Garg <ashish.garg@digimantra.com>
2023-07-07 18:29:40 +0530

    updated

--- src/Pages/productInfo.js
+++ src/Pages/productInfo.js
@@ -19,253 +19,260 @@ const Product = () => {
       </header>
       <main className="main-container">
         <div className="top-body">
-          <div className="top-left-body">
-            <div className="img-body">
-              <img
-                src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-16-2.jpg"
-                alt="product-16-2"
-              />
-            </div>
-            <div className="card-list">
-              <img
-                className="item"
-                src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-3.jpg"
-                alt="1"
-              />
-              <img
-                className="item"
-                src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-4.jpg"
-                alt="2"
-              />
-              <img
-                className="item"
-                src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-5.jpg"
-                alt="3"
-              />
-              <img
-                className="item"
-                src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-6.jpg"
-                alt="4"
-              />
-            </div>
-          </div>
-          <div className="top-mid-body">
-            <p>Sale Off</p>
-            <h2>
-              Seeds of Change <br />
-              Organic Quinoa, Brown
-            </h2>
-            <Star />
-            <h1>
-              $38
-              <div className="offering">
-                <p>26% off</p>
-                <h3>$52</h3>
-              </div>
-            </h1>
-            <div className="short-desc">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                <br />
-                Aliquam rem officia, corrupti reiciendis minima nisi modi,
-                quasi,
-                <br /> odio minus dolore impedit fuga eum eligendi.
-              </p>
-            </div>
-            <div className="size-weight-chart">
-              <ul>
-                <strong> Size/weight :</strong>
-                <li>50g</li>
-                <li style={{ color: "white", background: "#3bb77e" }}>60g</li>
-                <li>80g</li>
-                <li>100g</li>
-                <li>150g</li>
-              </ul>
-            </div>
-            <div className="extarlinks">
-              <input type="number" defaultValue={1} min={1} />
-              <button type="submit">Add to cart</button>
-              <a href="/">
-                <img src={heart} alt="wishlist" />
-              </a>
-              <a href="/">
-                <img src={shuffle} alt="shuffle" height={"42px"} />
-              </a>
-            </div>
-            <div className="product-info">
-              <ul>
-                <li>
-                  Type: <p>Organic</p>
-                </li>
-                <li>
-                  MFG: <p>July 07, 2023</p>
-                </li>
-                <li>
-                  Life: <p>70days</p>
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  Sku: <p>FWM15VKT</p>
-                </li>
-                <li>
-                  Tags: <p>Snack, Organic, Brown</p>
-                </li>
-                <li>
-                  Stock: <p>8 Items In Stock</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="top-right-body">
-            <div className="catagory">
-              <h4 className="cat-head">Catagory</h4>
-              <ul>
-                <li>
-                  <a href="/">
-                    <img
-                      src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-1.svg"
-                      alt="cat-1"
-                    />
-                    Milk & Dairies
-                  </a>
-                  <p>30</p>
-                </li>
-                <li>
-                  <a href="/">
-                    <img
-                      src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-2.svg"
-                      alt="cat-2"
-                    />
-                    Clothing
-                  </a>
-                  <p>35</p>
-                </li>
-                <li>
-                  <a href="/">
-                    <img
-                      src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-3.svg"
-                      alt="cat-3"
-                    />
-                    Pet Foods
-                  </a>
-                  <p>42</p>
-                </li>
-                <li>
-                  <a href="/">
-                    <img
-                      src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-4.svg"
-                      alt="cat-4"
-                    />
-                    Baking Material
-                  </a>
-                  <p>68</p>
-                </li>
-                <li>
-                  <a href="/">
-                    <img
-                      src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-5.svg"
-                      alt="cat-5"
-                    />
-                    Fresh Fruit
-                  </a>
-                  <p>87</p>
-                </li>
-              </ul>
-            </div>
-            <div className="price">
-              <h4 className="fill-by">Fill by Price</h4>
-              <input type="range" />
-              <br />
-              <div className="range">
-                <p>
-                  From: <strong>$0</strong>
-                </p>
-                <p>
-                  To: <strong>$2,000</strong>
-                </p>
-              </div>
-              <label>Color</label>
-              <div className="chkbox">
-                <input type="checkbox" />
-                <label>Red (56)</label>
-                <br />
-                <input type="checkbox" />
-                <label>Green (78)</label>
-                <br />
-                <input type="checkbox" />
-                <label>Blue (54)</label>
-                <br />
-              </div>
-              <label>Item Condition</label>
-              <div className="chkbox">
-                <input type="checkbox" />
-                <label>New (1506)</label>
-                <br />
-                <input type="checkbox" />
-                <label>Refurbished (27)</label>
-                <br />
-                <input type="checkbox" />
-                <label>Used (45)</label>
-                <br />
-              </div>
-              <button type="submit">
-                <img src={filter} alt="filter" />
-                Filter
-              </button>
-            </div>
-            <div className="new-products">
-              <h4>New products</h4>
-              <div className="single-new">
+          <div className="left-fix">
+            <div className="top-left-body">
+              <div className="img-body">
                 <img
+                  src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-16-2.jpg"
+                  alt="product-16-2"
+                />
+              </div>
+              <div className="card-list">
+                <img
+                  className="item"
                   src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-3.jpg"
-                  alt="thumbnail-1"
-                  height={"80px"}
+                  alt="1"
                 />
-                <div className="new-info">
-                  <h5>Chen Cardigan</h5>
-                  <p>
-                    $99.50
-                    <Star />
-                  </p>
-                </div>
-              </div>
-              <div className="single-new">
                 <img
+                  className="item"
                   src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-4.jpg"
-                  alt="thumbnail-2"
-                  height={"80px"}
+                  alt="2"
                 />
-                <div className="new-info">
-                  <h5>Chen Sweater</h5>
-                  <p>
-                    $89.50
-                    <Star />
-                  </p>
-                </div>
-              </div>
-              <div className="single-new">
                 <img
+                  className="item"
                   src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-5.jpg"
-                  alt="thumbnail-3"
-                  height={"80px"}
+                  alt="3"
                 />
-                <div className="new-info">
-                  <h5>Colorful Jacket</h5>
-                  <p>
-                    $25
-                    <Star />
-                  </p>
-                </div>
+                <img
+                  className="item"
+                  src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-6.jpg"
+                  alt="4"
+                />
               </div>
             </div>
-            <div className="banner-img">
-              {/* <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-11.png" alt="banner-11" height={'340px'} /> */}
-              <div className="banner-text">
-                <span>Organic</span>
-                <h4>Save 17% <br />
-                on <strong>Organic</strong><br />
-                Juice</h4>
+            <div className="top-mid-body">
+              <p>Sale Off</p>
+              <h2>
+                Seeds of Change <br />
+                Organic Quinoa, Brown
+              </h2>
+              <Star />
+              <h1>
+                $38
+                <div className="offering">
+                  <p>26% off</p>
+                  <h3>$52</h3>
+                </div>
+              </h1>
+              <div className="short-desc">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  <br />
+                  Aliquam rem officia, corrupti reiciendis minima nisi modi,
+                  quasi,
+                  <br /> odio minus dolore impedit fuga eum eligendi.
+                </p>
+              </div>
+              <div className="size-weight-chart">
+                <ul>
+                  <strong> Size/weight :</strong>
+                  <li>50g</li>
+                  <li style={{ color: "white", background: "#3bb77e" }}>60g</li>
+                  <li>80g</li>
+                  <li>100g</li>
+                  <li>150g</li>
+                </ul>
+              </div>
+              <div className="extarlinks">
+                <input type="number" defaultValue={1} min={1} />
+                <button type="submit">Add to cart</button>
+                <a href="/">
+                  <img src={heart} alt="wishlist" />
+                </a>
+                <a href="/">
+                  <img src={shuffle} alt="shuffle" height={"42px"} />
+                </a>
+              </div>
+              <div className="product-info">
+                <ul>
+                  <li>
+                    Type: <p>Organic</p>
+                  </li>
+                  <li>
+                    MFG: <p>July 07, 2023</p>
+                  </li>
+                  <li>
+                    Life: <p>70days</p>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    Sku: <p>FWM15VKT</p>
+                  </li>
+                  <li>
+                    Tags: <p>Snack, Organic, Brown</p>
+                  </li>
+                  <li>
+                    Stock: <p>8 Items In Stock</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="right-fix">
+            <div className="top-right-body">
+              <div className="catagory">
+                <h4 className="cat-head">Catagory</h4>
+                <ul>
+                  <li>
+                    <a href="/">
+                      <img
+                        src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-1.svg"
+                        alt="cat-1"
+                      />
+                      Milk & Dairies
+                    </a>
+                    <p>30</p>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <img
+                        src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-2.svg"
+                        alt="cat-2"
+                      />
+                      Clothing
+                    </a>
+                    <p>35</p>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <img
+                        src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-3.svg"
+                        alt="cat-3"
+                      />
+                      Pet Foods
+                    </a>
+                    <p>42</p>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <img
+                        src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-4.svg"
+                        alt="cat-4"
+                      />
+                      Baking Material
+                    </a>
+                    <p>68</p>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <img
+                        src="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-5.svg"
+                        alt="cat-5"
+                      />
+                      Fresh Fruit
+                    </a>
+                    <p>87</p>
+                  </li>
+                </ul>
+              </div>
+              <div className="price">
+                <h4 className="fill-by">Fill by Price</h4>
+                <input type="range" />
+                <br />
+                <div className="range">
+                  <p>
+                    From: <strong>$0</strong>
+                  </p>
+                  <p>
+                    To: <strong>$2,000</strong>
+                  </p>
+                </div>
+                <label>Color</label>
+                <div className="chkbox">
+                  <input type="checkbox" />
+                  <label>Red (56)</label>
+                  <br />
+                  <input type="checkbox" />
+                  <label>Green (78)</label>
+                  <br />
+                  <input type="checkbox" />
+                  <label>Blue (54)</label>
+                  <br />
+                </div>
+                <label>Item Condition</label>
+                <div className="chkbox">
+                  <input type="checkbox" />
+                  <label>New (1506)</label>
+                  <br />
+                  <input type="checkbox" />
+                  <label>Refurbished (27)</label>
+                  <br />
+                  <input type="checkbox" />
+                  <label>Used (45)</label>
+                  <br />
+                </div>
+                <button type="submit">
+                  <img src={filter} alt="filter" />
+                  Filter
+                </button>
+              </div>
+              <div className="new-products">
+                <h4>New products</h4>
+                <div className="single-new">
+                  <img
+                    src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-3.jpg"
+                    alt="thumbnail-1"
+                    height={"80px"}
+                  />
+                  <div className="new-info">
+                    <h5>Chen Cardigan</h5>
+                    <p>
+                      $99.50
+                      <Star />
+                    </p>
+                  </div>
+                </div>
+                <div className="single-new">
+                  <img
+                    src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-4.jpg"
+                    alt="thumbnail-2"
+                    height={"80px"}
+                  />
+                  <div className="new-info">
+                    <h5>Chen Sweater</h5>
+                    <p>
+                      $89.50
+                      <Star />
+                    </p>
+                  </div>
+                </div>
+                <div className="single-new">
+                  <img
+                    src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-5.jpg"
+                    alt="thumbnail-3"
+                    height={"80px"}
+                  />
+                  <div className="new-info">
+                    <h5>Colorful Jacket</h5>
+                    <p>
+                      $25
+                      <Star />
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="banner-img">
+                {/* <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-11.png" alt="banner-11" height={'340px'} /> */}
+                <div className="banner-text">
+                  <span>Organic</span>
+                  <h4>
+                    Save 17% <br />
+                    on <strong>Organic</strong>
+                    <br />
+                    Juice
+                  </h4>
+                </div>
               </div>
             </div>
           </div>
